9c235ac8b8141feb23a228f364bd3fa4308ebc5c
Gwyl <Gwylim.l.richards@gmail.com>
2022-06-20 12:27:20 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,10 +4,22 @@ const input = process.argv;
 console.log(input);
 const movies = [];
 movies.push(input[2]);
+//one way
 if(input[2] === "add"){
     const movieObj = new Movie(input[3],input[4],input[5],input[6]);
     movieObj.add();
+    console.log(movieObj);
 }
 else if(input[2] ==='remove'){
     movieObj.remove();
 }
+
+//other way
+// if(input[2] === "add"){
+//     const movieObj1 = new moveBy(input[3],input[4]);
+//     movieObj1.add();
+//     const movieObj2 = new moveBy(input[5],input[6]);
+//     movieObj2.add();
+//     console.log(movieObj1);
+//     console.log(movieObj2);
+// }
